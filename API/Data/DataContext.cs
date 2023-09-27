89b0cf2799373130852c0ad98f374ed93a0be8cc
Vasile-Caspirovschi <97791123+Vasile-Caspirovschi@users.noreply.github.com>
2023-09-27 22:10:52 +0300

    Creating a simple API and setting up the angular client app.

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<AppUser> Users { get; set; }
+
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
