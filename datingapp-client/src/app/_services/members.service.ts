ed3a55c5e8b112802059982e8fde82038648895a
Vasile-Caspirovschi <97791123+Vasile-Caspirovschi@users.noreply.github.com>
2023-10-03 21:43:53 +0300

    Adding photo gallery

--- datingapp-client/src/app/_services/members.service.ts
+++ datingapp-client/src/app/_services/members.service.ts
@@ -18,7 +18,7 @@ export class MembersService {
   }
 
   getMember(username: string) {
-    return this.http.get<Member>(this.baseUrl + 'users' + username);
+    return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 
 }
