bc641aa917334329e62ba8f27e21d9603ae5c845
Vasile-Caspirovschi <97791123+Vasile-Caspirovschi@users.noreply.github.com>
2023-10-16 11:09:53 +0300

    AddedLikeEntity

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -10,5 +10,23 @@ namespace API.Data
         public DataContext(DbContextOptions options) : base(options)
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<UserLike>().HasKey(key => new { key.SourceUserId, key.LikedUserId });
+            modelBuilder.Entity<UserLike>()
+                .HasOne(s => s.SourceUser)
+                .WithMany(l => l.LikedUsers)
+                .HasForeignKey(s => s.SourceUserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<UserLike>()
+                .HasOne(s => s.LikedUser)
+                .WithMany(l => l.LikedByUsers)
+                .HasForeignKey(s => s.LikedUserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
